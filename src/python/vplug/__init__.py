0bcfcb5e707ec53c7996a8e8d625fd1264ca896c
Eugene Cherny <iam@oscii.ru>
2017-12-16 05:20:21 +0200

    [+] python interface for Vamp::Plugin

--- src/python/vplug/__init__.py
+++ src/python/vplug/__init__.py
@@ -1,7 +1,10 @@
+import inspect
+import sys
+
 from .vampinterface import *
 
 
 def find_plugins():
     return [c[1] for c
             in inspect.getmembers(sys.modules['vplug'], inspect.isclass)
-            if issubclass(c[1], VampPlugin) and c[1] is not VampPlugin]
+            if issubclass(c[1], VampPluginInterface) and c[1] is not VampPluginInterface]
--- src/python/vplug/vampinterface.py
+++ src/python/vplug/vampinterface.py
@@ -1,21 +1,137 @@
 from abc import ABCMeta, abstractmethod
-import inspect, sys
+from enum import Enum
+from typing import List, Dict
+
+import numpy as np
 
 
-class VampPlugin(metaclass=ABCMeta):
+class InputDomain(Enum):
+    Time = 0
+    Frequency = 1
+
+
+class SampleType(Enum):
+    OneSamplePerStep = 0,
+    FixedSampleRate = 1,
+    VariableSampleRate = 2
+
+
+class RealTime:
+    def __init__(self, sec: int = 0, nsec: int = 0):
+        self.sec = sec
+        self.nsec = nsec
+
+
+class OutputDescriptor:
+    def __init__(self,
+                 identifier: str,
+                 name: str,
+                 description: str = '',
+                 unit: str = 'unknown',
+                 has_fixed_bin_count: bool = False,
+                 bin_count: int = 0,
+                 bin_names: List[str] = None,
+                 has_known_extents: bool = False,
+                 min_value: float = 0,
+                 max_value: float = 0,
+                 is_quantized: bool = False,
+                 quantize_step: float = 0,
+                 sample_type: int = 0,
+                 sample_rate: float = 0,
+                 has_duration: bool = False):
+        if bin_names is None:
+            bin_names = []
+
+        self.identifier = identifier
+        self.name = name
+        self.description = description
+        self.unit = unit
+        self.has_fixed_bin_count = has_fixed_bin_count
+        self.bin_count = bin_count
+        self.bin_names = bin_names
+        self.has_known_extents = has_known_extents
+        self.min_value = min_value
+        self.max_value = max_value
+        self.is_quantized = is_quantized
+        self.quantize_step = quantize_step
+        self.sample_type = sample_type
+        self.sample_rate = sample_rate
+        self.has_duration = has_duration
+
+
+class Feature(object):
+    def __init__(self,
+                 had_timestamp: bool = False,
+                 timestamp: RealTime = RealTime(),
+                 has_duration: bool = False,
+                 duration: RealTime = RealTime(),
+                 values: List[float] = None,
+                 label: str = 'default_label'):
+        if values is None:
+            values = []
+        self.had_timestamp = had_timestamp
+        self.timestamp = timestamp
+        self.has_duration = has_duration
+        self.duration = duration
+        self.values = values
+        self.label = label
+
+
+class VampPluginInterface(metaclass=ABCMeta):
     @abstractmethod
     def __init__(self, sr):
+        pass
+
+    """ Plugin """
+
+    @abstractmethod
+    def get_identifier(self) -> str:
+        pass
+
+    @abstractmethod
+    def initialise(self, input_channels: int,
+                   step_size: int, block_size: int) -> bool:
         pass
 
     @abstractmethod
     def reset(self):
         pass
 
+    @abstractmethod
+    def get_input_domain(self) -> int:
+        pass
 
-class PlugExample(VampPlugin):
-    def __init__(self, sr):
-        self.sr = sr
+    @abstractmethod
+    def get_output_descriptors(self) -> List[OutputDescriptor]:
+        pass
 
-    def reset(self):
-        print("WOW, you’ve just reset me!")
-        print("And btw, I use sampling rate {}.".format(self.sr))
+    @abstractmethod
+    def process(self, input_buffers: List[np.array],
+                sec: int, nsec: int) -> Dict[int, List[Feature]]:
+        pass
+
+    @abstractmethod
+    def get_remaining_features(self) -> Dict[int, List[Feature]]:
+        pass
+
+    """ PluginBase """
+
+    @abstractmethod
+    def get_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_description(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_maker(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_copyright(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_plugin_version(self) -> int:
+        pass
