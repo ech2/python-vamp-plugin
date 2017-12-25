3473a34d1ab38785356bac215246fd80b779bce7
Eugene Cherny <iam@oscii.ru>
2017-12-25 08:29:06 +0200

    [fix] PythonPluginAdapter population at Python initialization

--- src/cpp/plugin.cpp
+++ src/cpp/plugin.cpp
@@ -3,25 +3,23 @@
 #include <list>
 #include "vamp/vamp.h"
 #include "pybind11/embed.h"
+#include "pybind11/stl.h"
 #include "PythonPluginAdapter.h"
 
-namespace py = pybind11;
 using namespace vplug;
 
-static std::vector<PythonPluginAdapter> pyPlugins;
-
-static void findPythonVampClasses(std::vector<PythonPluginAdapter> &plugs) {
-    auto m = py::module::import("vplug");
-    auto classes = m.attr("find_vamp_modules_from_path").cast<std::list<py::object>>();
-    for (auto &c : classes) {
-        plugs.push_back(std::move(PythonPluginAdapter{c}));
-    }
-}
+static std::vector<PythonPluginAdapter> *pyPlugins;
 
 static void pyInit() {
     try {
         py::initialize_interpreter();
-        findPythonVampClasses(pyPlugins);
+        auto m = py::module::import("vplug");
+        pyPlugins = new std::vector<PythonPluginAdapter>();
+        auto classes = m.attr("find_test_module")()
+                .cast<std::list<std::tuple<std::string, py::object>>>();
+        for (auto &c : classes) {
+            (*pyPlugins).push_back(std::move(PythonPluginAdapter{std::get<1>(c)}));
+        }
     } catch (std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
     }
@@ -35,9 +33,9 @@ const VampPluginDescriptor *vampGetPluginDescriptor(unsigned int version, unsign
     }
     pyInit();
 
-    if (index > pyPlugins.size() - 1) {
+    if (index > (*pyPlugins).size() - 1) {
         return nullptr;
     } else {
-        return pyPlugins[index].getDescriptor();
+        return (*pyPlugins)[index].getDescriptor();
     }
 }
